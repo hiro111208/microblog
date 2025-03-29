3c023c6ec98df7aee049e4bb0112a80a191e1f0a
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2025-03-29 22:12:35 +0900

    Add a text search functionality utilising Elasticsearch.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler, SMTPHandler
 
+from elasticsearch import Elasticsearch
 from flask import Flask, current_app, request
 from flask_babel import Babel
 from flask_babel import lazy_gettext as _l
@@ -54,6 +55,12 @@ def create_app(config_class=Config):
     from app.cli import bp as cli_bp
 
     app.register_blueprint(cli_bp)
+
+    app.elasticsearch = (
+        Elasticsearch([app.config["ELASTICSEARCH_URL"]])
+        if app.config["ELASTICSEARCH_URL"]
+        else None
+    )
 
     if not app.debug and not app.testing:
         if app.config["MAIL_SERVER"]:
